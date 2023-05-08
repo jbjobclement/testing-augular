fadbeeaeaa76d32d31066b0910891bea4283b16c
jbjobclement <clement@jbjob.com.tw>
2023-05-08 17:01:13 +0800

    feat: class and style binding
    topic
    - class and style binding
    
    modified
    - binding.component.html
    - binding.component.scss
    - binding.component.ts
    
    reference
    - https://angular.tw/guide/class-binding

--- src/app/components/pages/binding/binding.component.ts
+++ src/app/components/pages/binding/binding.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class BindingComponent {
   attrTesting = "attr testing"
+  cssClass1 = false
+  multipleClassName1 = "a b c"
+  multipleClassName2 = {
+    foo: true, bar: true
+  }
+  multipleClassName3 = ["a", "b", "c"]
+  singleStyleColor: string = "green"
+  multipleStyle: Partial<CSSStyleDeclaration> = {
+    color: "yellow",
+    background: "green"
+  };
+  toggleCssClass1(): void {
+    this.cssClass1 = !this.cssClass1;
+  }
 }
