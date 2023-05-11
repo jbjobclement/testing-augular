5fd84d7eb4894c7ee5aa775dc5fe83dca64d7a30
jbjobclement <clement@jbjob.com.tw>
2023-05-11 11:51:15 +0800

    feat: rxjs observables
    topic
    - observable
    - subscription
    
    modified
    - rxjs-testing.component.html
    - rxjs-testing.component.ts
    
    reference
    - https://rxjs.dev/guide/observable

--- src/app/components/pages/rxjs-testing/rxjs-testing.component.ts
+++ src/app/components/pages/rxjs-testing/rxjs-testing.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Subscription, fromEvent } from 'rxjs';
+import { Observable, Subscription, from, fromEvent } from 'rxjs';
 import { map, scan, throttleTime } from 'rxjs/operators';
 
 @Component({
@@ -11,7 +11,9 @@ import { map, scan, throttleTime } from 'rxjs/operators';
 export class RxjsTestingComponent {
   subscription!: Subscription;
   x: number  = 0;
+
   ngOnInit(){
+    // https://rxjs.dev/guide/overview
     this.subscription = fromEvent<MouseEvent>(document, 'click')
     .pipe(
       // 最小可觸發間隔
@@ -23,8 +25,69 @@ export class RxjsTestingComponent {
     )
     .subscribe((count) => {
       this.x = count;
-      console.log(`clicked ${count} times.`)
+      // console.log(`clicked ${count} times.`)
     })
+
+  }
+  
+  docObservableEX1() {
+    // https://rxjs.dev/guide/observable
+    // 該範例可顯示出Observable的同步與非同步操作。
+    const observable = new Observable<number>((subscriber) => {
+      subscriber.next(1);
+      subscriber.next(2);
+      subscriber.next(3);
+
+      try {
+        throw new Error()
+      } catch (e){
+        subscriber.error("testing error")
+      }
+      
+      setTimeout(() => {
+        subscriber.next(4);
+        subscriber.complete();
+      }, 1000);
+    });
+    
+    console.log('just before subscribe');
+
+    const subscription = observable.subscribe({
+      next(x: number) {
+        console.log('got value f' + x);
+      },
+      error(err) {
+        console.error('something wrong occurred: ' + err);
+      },
+      complete() {
+        console.log('done');
+      },
+    });
+    console.log('just after subscribe');
+    
+    subscription.unsubscribe();
+  }
+
+  docObservableEX2() {
+    const foo = new Observable((subscriber) => {
+      console.log('Hello');
+      subscriber.next(42);
+    });
+     
+    foo.subscribe((x) => {
+      console.log(x);
+    });
+
+    foo.subscribe((y) => {
+      console.log(y);
+    });
+  }
+
+  docObservableEX3() {
+    const observable = from([10, 20, 30]);
+    const subscription = observable.subscribe((x) => console.log(x));
+    // Later:
+    subscription.unsubscribe();
   }
 
   ngOnDestroy() {
